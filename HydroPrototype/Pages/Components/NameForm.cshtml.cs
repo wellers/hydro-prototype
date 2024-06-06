cf3995c990b213dbf92c15d09fdadd9e0a221827
wellers <paulwelbourne@gmail.com>
2024-06-06 22:00:32 +0100

    Add NameForm control.

---
+++ HydroPrototype/Pages/Components/NameForm.cshtml.cs
@@ -0,0 +1,9 @@
+using Hydro;
+
+namespace HydroPrototype.Pages.Components;
+
+public class NameForm : HydroComponent
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+}
